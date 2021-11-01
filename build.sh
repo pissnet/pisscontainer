ecbe5dd4aca8e30f5ccc763b93e6967cb0cb567e
Raphael Bertoche <bertocheraphael@gmail.com>
2021-11-01 04:21:48 -0300

    takes repo and branch from command line

--- build.sh
+++ build.sh
@@ -1,24 +1,40 @@
-#!/bin/bash -e
+#!/bin/bash
 
-if [ "$#" -t 1 ]; then
-	export BRANCH=piss60
+set -e
+
+if [ "$#" -ge 2 ]; then
+	export REPO="$1"
+	shift
 else
-	export BRANCH="$1"
+	export REPO=pissnet/pissircd
 fi
+if [ "$#" -ge 1 ]; then
+	export BRANCH="$1"
+	shift
+else
+	export BRANCH=piss60
+fi
+echo $REPO $BRANCH
 
 if wget --version 1>/dev/null 2>&1; then
-	wget "https://codeload.github.com/pissnet/pissircd/zip/refs/heads/$BRANCH" \
-		-O pissnet.zip
+	wget "https://codeload.github.com/$REPO/zip/refs/heads/$BRANCH" \
+		-O pissircd.zip
 elif curl --version 1>/dev/null 2>&1; then
-	curl "https://codeload.github.com/pissnet/pissircd/zip/refs/heads/$BRANCH" \
-		-O pissnet.zip
+	curl "https://codeload.github.com/$REPO/zip/refs/heads/$BRANCH" \
+		-O pissircd.zip
 else
 	echo "Can't download pissnet. Install _something_!"
 	exit -1
 fi
-REV=`unzip -z pissnet.zip | tail -1`
+
+REV=`unzip -z pissircd.zip | tail -1`
 SHORTREV=`echo "$REV" | dd bs=1 count=6`
 
+rm -rf tmp pissircd
+mkdir -p tmp
+unzip -qx pissircd.zip -d tmp
+
+mv tmp/* pissircd
 
 mkdir -p unrealircd
 mkdir -p data
@@ -39,7 +55,7 @@ podman build -f Containerfile_full_server --build-arg BRANCH="$BRANCH" \
 # 		--label REV="$SHORTREV"
 
 echo "Running..."
-podman run -it --name=pissnet --user=pissnet \
+podman run -it --name="$repo_$branch" --user=pissnet \
 		-p6667:6667 -p6697:6697 -p6900:6900 \
 		-p [::]:6900:6900 -p [::]:6667:6667 -p [::]:6697:6697 \
 		--label REV="$SHORTREV" \
